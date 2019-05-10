4fa68f9feb3b2f2d64c73f97f694822ed58e7b8b
medistriputra <50002064+medistriputra@users.noreply.github.com>
2019-05-10 20:30:43 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,25 @@
+<ul class="menu">
+	<li><a href="provinsi.php">Provinsi &raquo</a></li>
+	<li><a href="kabupaten.php">Kabupaten &raquo</a></li>
+	<li><a href="kecamatan.php">Kecamatan &raquo</a></li>
+</ul>
+
+<style type="text/css">
+	.menu li {
+		list-style: none;
+		float: left;
+	}
+	.menu a {
+		text-decoration: none;
+		color : #00adef;
+	}
+	.menu a:hover {
+		color:#000fff;
+	}
+	.menu li a {
+		padding: 10px;
+		margin: 10px;
+	}
+</style>
+
+<br>
